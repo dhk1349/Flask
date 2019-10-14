b3345d72be7b0b4796b98b2223bd6b34c89c183b
dhk1349 <dhk1349@naver.com>
2019-10-14 18:13:52 +0900

    .

--- vola/JBGR_main.py
+++ vola/JBGR_main.py
@@ -7,6 +7,9 @@ Created on Sat Oct 12 13:36:14 2019
 
 import sys
 from flask import Flask,render_template, g, request, redirect
+import volatility.conf as conf
+
+config = conf.ConfObject()
 app=Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
@@ -23,6 +26,11 @@ def pass_info():
     if sys.version_info < (2, 6, 0):
         sys.stderr.write("Volatility requires python version 2.6, please upgrade your python installation.")
         sys.exit(1)
+    config.add_option("INFO", default = None, action = "store_true",
+                  cache_invalidator = False,
+                  help = "Print information about all registered objects")
+    config.set_usage(usage = "Volatility - A memory forensics analysis platform.")
+    config.add_help_hook(list_plugins)
     
 
 if __name__=="__main__":
