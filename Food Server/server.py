b446d022e29900d02e4e15ae88e2eaeed71f1695
한동훈 <dhk1349@naver.com>
2021-01-21 22:06:50 +0900

    .

--- Food Server/server.py
+++ Food Server/server.py
@@ -7,12 +7,16 @@ Created on  2021
 
 import os
 import sys
-
+import sqlite3
 from flask import Flask, jsonify, request, send_file, render_template
 from werkzeug.utils import secure_filename
 import requests
 
 app = Flask(__name__)
+
+#connecting database
+conn=sqlite3.connect("./database/food.db")
+cur=conn.cursor()
 
 @app.route('/')
 def index():
@@ -21,16 +25,36 @@ def index():
 
 
 @app.route('/result', methods=['GET', 'POST'])
-def p3():
+def result():
+    if request.method == 'POST':
+        data=request.form
+        option=request.form['method']
+        
+        if option=="food":
+            query="select * from food where ingred=?"
+            
+        elif option=="ingred":
+            query="select * from ingred where food=?"
+        
+        cur.execute(query, (data['name']))
+        rows=cur.fetchall()
+    return render_template("result.html", result=rows)
 
-    
-    return render_template("result.html")
 
-
-@app.route('/page4')
-def p4():
-
-    return render_template("page4.html")
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        data=request.form
+        option=request.form['method']
+        
+        if option=="food":
+            query="insert into food(food, ingred) value(?,?)"
+            
+        elif option=="ingred":
+            query="insert into ingred(ingred, food) value(?,?)"
+        
+        cur.execute(query, ())
+    return render_template("add.html")
 
 @app.route('/about')
 def about():
